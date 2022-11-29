6d66204ee2f310f84a79a015cbea1048665f0ca8
Awo Moussa <amoussab@ualberta.ca>
2022-11-29 08:17:46 +0000

    made changes to tinyapp

--- express_server.js
+++ express_server.js
@@ -2,6 +2,7 @@ const express = require ("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
+app.set("view engine", "ejs");//tells the Express app to use EJS as its templating engine
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
