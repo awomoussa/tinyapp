e7fd84be1afc94edb566ea8d3369a4169691b64d
Awo Moussa <amoussab@ualberta.ca>
2022-11-29 10:06:57 +0000

    Added GET and POST routes

--- express_server.js
+++ express_server.js
@@ -1,6 +1,8 @@
 const express = require ("express");
 const app = express();
 const PORT = 8080; // default port 8080
+function generateRandomString() {} //generating a unique short URL (6 random characters) 
+
 
 app.set("view engine", "ejs");//tells the Express app to use EJS as its templating engine
 
@@ -8,6 +10,13 @@ const urlDatabase = { //used to keep track of all the URLs and their shortened f
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 }
+
+app.use(express.urlencoded({ extended: true }));
+
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+});
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -27,6 +36,10 @@ app.get("/urls.json", (req, res) => {
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 }); //curl not working 
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
 
 app.get("/urls/:id", (req, res) => {
   const templateVars = { id: req.params.id, longURL:  ("b2xVn2", "9sm5xK") /* What goes here? */ };
